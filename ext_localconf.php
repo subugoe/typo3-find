8d1954fe28f16e566572ff07d65a6201f269e663
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-04-30 17:39:18 +0200

    Removed detail plugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,22 +7,12 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY, // The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
 	'SolrFrontend', // A unique name of the plugin in UpperCamelCase
 	array ( // An array holding the controller-action-combinations that are accessible
-		'Search' => 'index', // The first controller and its first action will be the default
+		'Search' => 'index, detail', // The first controller and its first action will be the default
 	),
 	array ( // An array holding the controller-action-combinations that are accessible
-		'Search' => 'index', // The first controller and its first action will be the default
+		'Search' => 'index, detail', // The first controller and its first action will be the default
 	)
 );
 
-Tx_Extbase_Utility_Extension::configurePlugin(
-	$_EXTKEY, // The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
-	'SolrFrontendDetail', // A unique name of the plugin in UpperCamelCase
-	array ( // An array holding the controller-action-combinations that are accessible
-		'Search' => 'detail', // The first controller and its first action will be the default
-	),
-	array ( // An array holding the controller-action-combinations that are accessible
-		'Search' => 'detail', // The first controller and its first action will be the default
-	)
-);
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['pagebrowse']['additionalMarkers'][] = 'Tx_SolrFrontend_Hooks_PageBrowse->addAdditionalMarkers';
