7921730418782eaffd843dbeea1e2d3fe56c2403
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-05-14 19:10:41 +0200

    new ViewHelper: ValueForKey
    to read _variable_ items out of arrays
    
    supports JSON output (optional)

---
+++ Classes/ViewHelpers/ValueForKeyViewHelper.php
@@ -0,0 +1,58 @@
+<?php
+/*******************************************************************************
+ * Copyright notice
+ *
+ * Copyright 2013 Sven-S. Porst, Göttingen State and University Library
+ *                <porst@sub.uni-goettingen.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+
+/**
+ * View Helper to return the value of a key in an array.
+ */
+class Tx_SolrFrontend_ViewHelpers_ValueForKeyViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+
+	/**
+	 * Registers own arguments.
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerArgument('array', 'array', 'The array to extract the value from', True);
+		$this->registerArgument('key', 'string', 'The key to extract the value for', True);
+		$this->registerArgument('format', 'string', 'The format to return the value in ("text", "json")', False, 'text');
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function render() {
+		$result = $this->arguments['array'][$this->arguments['key']];
+
+		if ($this->arguments['format'] === 'json') {
+			$result = json_encode($result);
+		}
+
+		return $result;
+	}
+
+}
