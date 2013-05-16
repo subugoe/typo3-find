29ed53a11c5e8bdec69fa5269f86f78daf68052c
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-05-16 19:49:11 +0200

    Germania Sacra display improvements

--- Resources/Private/germania-sacra/configuration.ts
+++ Resources/Private/germania-sacra/configuration.ts
@@ -4,17 +4,28 @@ plugin.tx_solrfrontend {
       host = 127.0.0.1
      # host = vlib.sub.uni-goettingen.de
       port = 8080
-      path = /solr/gs
+      path = /solr/germania-sacra
     }
+	queryFields {
+		10.id = bistum
+		15 {
+			id = notbistum
+			query = NOT bistum:###term###
+			type = text
+		}
+		20 >
+		30 >
+		40 >
+	}
     standardFields {
       title = kloster
       snippet = uebersetzung
     }
     facets {
-      10 = orden
+      10 = orden_facet
       20 = orden_jahr50
-      30 = bistum
-      40 = land
+      30 = bistum_facet
+      40 = land_facet
     }
     facetDisplayCount {
       orden_jahr50 = 20000
@@ -22,6 +33,7 @@ plugin.tx_solrfrontend {
     additionalFilters {
       1 = typ:kloster
     }
+	CSSPaths.20 = EXT:solr_frontend/Resources/Private/germania-sacra/germania-sacra.css
   }
   view {
     templateRootPath = EXT:solr_frontend/Resources/Private/germania-sacra/Templates/
