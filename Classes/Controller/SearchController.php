80d586db1d7fdc21d72e221a995a42c8c2676c1e
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-05-24 10:38:44 +0200

    Autosuggest with dumb and not nice query

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -273,7 +273,13 @@ class Tx_SolrFrontend_Controller_SearchController extends Tx_Extbase_MVC_Control
 	 */
 	public function autoCompleteAction() {
 		$searchTerm = filter_var($_GET['term'], FILTER_SANITIZE_STRING);
-		$this->view->assign('searchTerm', $searchTerm);
+
+		$query = $this->solr->createSelect();
+
+		$query->setQuery($searchTerm);
+
+		$results = $this->solr->select($query)->getResponse()->getBody();
+		$this->view->assign('results', $results);
 	}
 
 	/**
