d847fbedb6d958df4eabdde8e1e7c3f1e0eebb9d
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-05-24 10:07:27 +0200

    Avoid caching in autocomplete

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,7 +18,10 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY, // The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
 	'AutoComplete', // A unique name of the plugin in UpperCamelCase
 	array ( // An array holding the controller-action-combinations that are accessible
-		'Search' => 'autocomplete', // The first controller and its first action will be the default
+		'Search' => 'autocomplete',
+	),
+	array (
+		'Search' => 'autocomplete',
 	)
 );
 
