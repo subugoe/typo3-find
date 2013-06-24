b9a3e23c59be9719392d4cce8e698aed1b42f530
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-06-24 15:43:09 +0200

    add basic configuration for HANS Solr Index

---
+++ Resources/Private/hans/configuration.ts
@@ -0,0 +1,56 @@
+plugin.tx_solrfrontend {
+	settings {
+		connection {
+			host = ssgfi1.sub.uni-goettingen.de
+			port = 8081
+			path = /solr/hans
+		}
+		queryFields {
+			10 {
+				extended = 1
+				id = title
+				type = Text
+			}
+			20 {
+				extended = 1
+				id = pers
+				type = Text
+			}
+			30 {
+				extended = 1
+				id = key
+				type = Text
+			}
+			40 {
+				extended = 1
+				id = date
+				type = Text
+			}
+		}
+		sort {
+		}
+		standardFields {
+			title = Gesamttitel
+			snippet = Regest
+		}
+		facets {
+			10 {
+				id = typ
+				field = Satzart
+			}
+		}
+		highlight {
+			fields.1 = *
+			query = ###term###
+			useQueryTerms = 1
+			useFacetTerms = 1
+		}
+		additionalFilters {
+		}
+
+	}
+	view {
+	}
+}
+
+plugin.tx_pagebrowse_pi1.enableMorePages = 1
