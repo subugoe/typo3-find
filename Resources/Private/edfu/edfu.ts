f68367a730ec9c53cfc2d77f4ac2fd58cf12ab31
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-07-03 11:18:21 +0200

    edfu: Baseline

---
+++ Resources/Private/edfu/edfu.ts
@@ -0,0 +1,49 @@
+plugin.tx_solrfrontend {
+	settings {
+		connection {
+			host = 127.0.0.1
+			port = 8080
+			path = /solr/edfu
+		}
+		queryFields {
+			10 {
+				extended = 1
+				id = transliteration
+				type = Text
+			}
+		}
+		sort {
+		}
+		standardFields {
+			title = transliteration
+			snippet = uebersetzung
+		}
+		facets {
+			10 {
+				id = typ
+				field = typ
+			}
+			20 {
+				id = band
+				field = band
+				sortOrder = index
+				displayDefault = 8
+			}
+		}
+		highlight {
+			fields.1 = *
+			query = ###term###* OR ###term###
+			useQueryTerms = 1
+			useFacetTerms = 1
+		}
+
+		CSSPaths.50 = EXT:solr_frontend/Resources/Private/edfu/edfu.css
+
+		languageRootPath = EXT:solr_frontend/Resources/Private/edfu/Language/
+	}
+	view {
+		templateRootPath = EXT:solr_frontend/Resources/Private/edfu/Templates/
+	}
+}
+
+plugin.tx_pagebrowse_pi1.enableMorePages = 1
