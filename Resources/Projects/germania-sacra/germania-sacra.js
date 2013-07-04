156b9524549d53bdab75ebf4c6309b57f9af564b
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-07-04 17:33:47 +0200

    germania-sacra: Logic for using order specific Marker icons

--- Resources/Projects/germania-sacra/germania-sacra.js
+++ Resources/Projects/germania-sacra/germania-sacra.js
@@ -66,27 +66,75 @@ var germaniaSacra = (function () {
 
 
 	var addNearby = function (map, queryURL) {
+		var iconForStandort = function (standortInfo, ordenInfos) {
+			var icon = 'http://maps.google.com/mapfiles/kml/paddle/red-circle-lv.png';
+
+			var matchingOrden = [];
+			for (var ordenIndex in ordenInfos) {
+				var ordenInfo = ordenInfos[ordenIndex];
+				if (standortInfo.von < ordenInfo.bis && ordenInfo.von < standortInfo.bis) {
+					matchingOrden.push(ordenInfo);
+				}
+			}
+			if (matchingOrden.length === 1 && matchingOrden[0].graphik !== '') {
+				// Unique orden with an icon: use it.
+				var fileName = matchingOrden[0].graphik;
+				var iconURL = 'http://vlib.sub.uni-goettingen.de/test/typo3conf/ext/find/Resources/Projects/germania-sacra/Ordenssymbole/' + fileName + '.png';
+				var size = new google.maps.Size(20, 30);
+				var origin = new google.maps.Point(0,0);
+				var anchor = new google.maps.Point(10, 30);
+				icon = new google.maps.MarkerImage(iconURL, size, origin, anchor);
+			}
+
+			return icon;
+		};
+
 		jQuery.getJSON(queryURL, function (data) {
 			var results = data.response.docs;
 			for (var docIndex in results) {
 				var result = results[docIndex];
 				if (result.id !== klosterID) {
+					var standorte = [];
 					var points = result.koordinaten;
-					for (var pointIndex in points) {
-						var coordinateStrings = points[pointIndex].split(',');
-						var lat = parseFloat(coordinateStrings[0]);
-						var long = parseFloat(coordinateStrings[1]);
-						var point = new google.maps.LatLng(lat,long);
-						var title = result.kloster;
-						var icon = 'http://maps.google.com/mapfiles/kml/paddle/red-circle-lv.png';
-						var orden_graphik = result.orden_graphik[pointIndex];
-						if (orden_graphik !== '') {
-							var iconURL = 'http://vlib.sub.uni-goettingen.de/test/typo3conf/ext/find/Resources/Projects/germania-sacra/Ordenssymbole/' + orden_graphik + '.png';
-							var size = new google.maps.Size(20, 31);
-							var origin = new google.maps.Point(0,0);
-							var anchor = new google.maps.Point(10, 31);
-							icon = new google.maps.MarkerImage(iconURL, size, origin, anchor);
+					console.log(result);
+					if (points) {
+						for (var pointIndex in points) {
+							var coordinateStrings = points[pointIndex].split(',');
+							var lat = parseFloat(coordinateStrings[0]);
+							var long = parseFloat(coordinateStrings[1]);
+
+							standorte.push({
+								'lat': lat,
+								'long': long,
+								'von': result.standort_von_von[pointIndex],
+								'bis': result.standort_bis_bis[pointIndex],
+								'ort': result.ort[pointIndex]
+							});
 						}
+					}
+
+					var ordenInfos = [];
+					var orden = result.orden;
+					if (orden) {
+						for (var ordenIndex in orden) {
+							var graphik;
+							if (result.orden_graphik) {
+								graphik = result.orden_graphik[ordenIndex];
+							}
+							ordenInfos.push({
+								'orden': orden[ordenIndex],
+								'von': result.orden_von_von[ordenIndex],
+								'bis': result.orden_bis_bis[ordenIndex],
+								'graphik': graphik
+							});
+						}
+					}
+
+					for (var standortIndex in standorte) {
+						var standort = standorte[standortIndex];
+						var point = new google.maps.LatLng(standort.lat, standort.long);
+						var title = result.kloster;
+						var icon = iconForStandort(standort, ordenInfos);
 
 						var marker = new google.maps.Marker({
 							'map': map,
