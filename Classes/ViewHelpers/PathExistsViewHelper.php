1687075b16a68be3b0cb58d709c662e8a613c5f1
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-07-08 19:39:55 +0200

    Add pathExists View Helper

---
+++ Classes/ViewHelpers/PathExistsViewHelper.php
@@ -0,0 +1,52 @@
+<?php
+/*******************************************************************************
+ * Copyright notice
+ *
+ * Copyright 2013 Sven-S. Porst, Göttingen State and University Library
+ *                <porst@sub.uni-goettingen.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+
+/**
+ * View Helper to determine whether the given path exists in the file system.
+ */
+class Tx_Find_ViewHelpers_PathExistsViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+
+	/**
+	 * Registers own arguments.
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerArgument('path', 'string', 'the path to check the existence of', TRUE);
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function render() {
+		return file_exists(PATH_site . $this->arguments['path']);
+	}
+
+}
+
+?>
