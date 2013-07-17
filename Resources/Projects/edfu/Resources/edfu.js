952c132e7d995d168ed2cbea36faf6d1bffa125f
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-07-17 11:23:20 +0200

    edfu: add powerzoomer JS for formular Template

--- Resources/Projects/edfu/Resources/edfu.js
+++ Resources/Projects/edfu/Resources/edfu.js
@@ -3,52 +3,82 @@
  *
  * 2013 Sven-S. Porst, SUB Göttingen <porst@sub.uni-goettingen.de>
  */
-var germaniaSacra = (function () {
-    var onSlide = function (event, ui) {
-        var filterStrings = [];
-        jQuery('.ui-slider').each( function () {
-            var value = $(this).slider('value');
-            filterStrings.push(this.id.split('-')[1] + '(' + value + '%)');
-        });
-        var filterString = filterStrings.join(' ');
+var edfu = (function () {
+	var onSlide = function (event, ui) {
+		var filterStrings = [];
+		jQuery('.ui-slider').each( function () {
+			var value = jQuery(this).slider('value');
+			filterStrings.push(this.id.split('-')[1] + '(' + value + '%)');
+		});
+		var filterString = filterStrings.join(' ');
 
-        var jFotorama = jQuery('.fotorama__stage__shaft');
-        jFotorama.css({'filter': filterString, '-webkit-filter': filterString});
-    };
-
-
-    jQuery.fn.fotoramaListAdapter = function () {
-        this.each( function () {
-            var html = '';
-
-            jQuery('> li', this).each(function () {
-                html += jQuery(this).html();
-            });
-
-            jQuery(this).html(html);
-        });
-
-        return this;
-    };
+		var jFotorama = jQuery('.fotorama__stage__shaft, .powerzoomer .inner');
+		jFotorama.css({'filter': filterString, '-webkit-filter': filterString});
+	};
 
 
+	jQuery.fn.fotoramaListAdapter = function () {
+		this.each(function () {
+			var html = '';
 
-    jQuery(function () {
-        // Initialise fotorama slideshow.
-        jQuery('ul.fotorama')
-            .fotoramaListAdapter()
-            .fotorama();
+			jQuery('> li', this).each(function () {
+				html += jQuery(this).html();
+			});
 
-        // Initialise slider for image settings.
-        jQuery('.slider').slider({
-            min: 0,
-            max: 200,
-            value: 100,
-            slide: onSlide
-        });
-    });
+			jQuery(this).html(html);
+		});
 
-    return {
-    };
+		return this;
+	};
+
+
+	var zoomTimeout;
+
+	var setupZoom = function (event) {
+		jImage = jQuery('.fotorama__stage__frame.fotorama__active img');
+
+		if (jImage.length > 0) {
+			jImage.addpowerzoom({
+				defaultpower: 2,
+				powerrange: [1.5, 7],
+				largeimage: null,
+				magnifiersize: [200,200]
+			});
+			console.log('added zoom');
+		}
+		else {
+			if (!zoomTimeout) {
+				console.log('setting timeout');
+				zoomTimeout = setTimeout(setupZoom, 500);
+			}
+			else {
+				zoomTimeout = undefined;
+			}
+		}
+
+
+	};
+
+
+	jQuery(function () {
+		// Initialise fotorama slideshow.
+        var jFotorama = jQuery('ul.fotorama');
+        jFotorama.on('fotorama:didShow', function (event) {
+      			setupZoom(event);
+        })
+
+		jFotorama.fotoramaListAdapter().fotorama();
+
+		// Initialise slider for image settings.
+		jQuery('.slider').slider({
+			min: 0,
+			max: 200,
+			value: 100,
+			slide: onSlide
+		});
+	});
+
+	return {
+	};
 
 })();
