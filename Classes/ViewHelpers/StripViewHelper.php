040c4d44fa81b4df611ee00072a437109e9df985
Sven-S. Porst <ssp-web@earthlingsoft.net>
2013-09-24 08:53:01 +0200

    add Strip View Helper

---
+++ Classes/ViewHelpers/StripViewHelper.php
@@ -0,0 +1,65 @@
+<?php
+/*******************************************************************************
+ * Copyright notice
+ *
+ * Copyright 2013 Sven-S. Porst, Göttingen State and University Library
+ *                <porst@sub.uni-goettingen.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+
+/**
+ * View Helper to strip leading and trailing whitespace or other characters from a given string.
+ */
+class Tx_Find_ViewHelpers_StripViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+
+	/**
+	 * Registers own arguments.
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerArgument('string', 'string', 'The string to strip leading and trailing whitespace from; If not given, the tag content is used', FALSE, NULL);
+		$this->registerArgument('strip', 'string', 'The characters to strip from the string; If not given, defaults to standard PHP whitespace setting', FALSE, NULL);
+	}
+
+
+	/**
+	 * @return array
+	 */
+	public function render() {
+		$string = $this->arguments['string'];
+		if ($string === NULL) {
+			$string = $this->renderChildren();
+		}
+
+		if ($this->arguments['strip'] === NULL) {
+			$string = trim($string);
+		}
+		else {
+			$string = trim($string, $this->arguments['strip']);
+		}
+
+		return $string;
+	}
+
+}
+
+?>
