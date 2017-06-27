39afa3e84b89a471bc1689d3ba93452cf680950d
ipf <ipf@users.noreply.github.com>
2017-06-27 13:55:23 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-7.0.99',
-            'typo3' => '6.2.0-8.99.99',
+            'typo3' => '7.6.0-8.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
