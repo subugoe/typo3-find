67802e8834be7602461e70616b5fb7949e954b24
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2021-11-29 11:39:46 +0100

    Apply rector rules

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,7 @@
 <?php
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'Subugoe.find',
+    'Find',
     'find',
     'find'
 );
--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Core\Configuration\Option;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
+use Ssch\TYPO3Rector\Set\Typo3SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -13,11 +14,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PATHS, [
         __DIR__.'/Classes',
         __DIR__.'/Tests',
+        __DIR__.'/Configuration',
+        __DIR__.'/Resources',
     ]);
 
     // Define what rule sets will be applied
     $containerConfigurator->import(SetList::CODING_STYLE);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
     $containerConfigurator->import(SetList::PHP_74);
+    $containerConfigurator->import(Typo3SetList::TYPO3_104);
+    $containerConfigurator->import(Typo3SetList::TYPOSCRIPT_CONDITIONS_104);
 
     // get services (needed for register a single rule)
     // $services = $containerConfigurator->services();
